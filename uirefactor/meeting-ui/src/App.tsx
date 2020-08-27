185d37f38d2c89835db3b11203c7cef98b0133fe
Devin Checa <devincheca@gmail.com>
2020-08-26 21:52:58 -0700

    now to get navigation in place

--- uirefactor/meeting-ui/src/App.tsx
+++ uirefactor/meeting-ui/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import DatePickerDiv from './components/DatePickerDiv/DatePickerDiv';
 import NavBar from './components/NavBar/NavBar';
+import 'bulma/css/bulma.css'
 
 function App() {
   return (
--- uirefactor/meeting-ui/src/components/DatePickerDiv/DatePickerDiv.tsx
+++ uirefactor/meeting-ui/src/components/DatePickerDiv/DatePickerDiv.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import './DatePickerDiv.css';
-
-function DatePickerDiv() {
-  return (
-    <div>
-        from datepicker
-    </div>
-  );
+import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+ 
+class DatePickerDiv extends React.Component {
+  state = {
+    chosenDate: new Date()
+  };
+ 
+  handleChange = (date:any) => {
+    this.setState({
+      chosenDate: date
+    });
+  };
+ 
+  render() {
+    return (
+      <DatePicker
+        selected={this.state.chosenDate}
+        onChange={this.handleChange}
+      />
+    );
+  }
 }
-
 export default DatePickerDiv;
