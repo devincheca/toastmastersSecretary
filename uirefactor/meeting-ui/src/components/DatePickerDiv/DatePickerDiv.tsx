93f37716429d282352ed29e18a98f711ad3805ff
Devin Checa <devincheca@gmail.com>
2020-08-26 21:11:15 -0700

    looks like I'll just use this then

---
+++ uirefactor/meeting-ui/src/components/DatePickerDiv/DatePickerDiv.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DatePickerDiv.css';
+
+function DatePickerDiv() {
+  return (
+    <div>
+        from datepicker
+    </div>
+  );
+}
+
+export default DatePickerDiv;
